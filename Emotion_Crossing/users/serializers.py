a424f4db48976a9b4d67fd227a390692ca12291e
bluelion10 <01cw@naver.com>
2025-07-05 18:16:24 +0900

    fix: 회원가입 시 Character UUID를 FK로 정상 연결되도록 UserSerializer 수정

--- Emotion_Crossing/users/serializers.py
+++ Emotion_Crossing/users/serializers.py
@@ -8,10 +8,16 @@ class CharacterSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     profile_character = CharacterSerializer(read_only=True)
-    profile_character_id = serializers.UUIDField(
+
+    # UUIDField 가 아니라 PrimaryKeyRelatedField로 바꿔야 함. 
+    # UUIDField만 쓰면 profile_character_id가 UUID를 받기는 하지만 
+    # character 객체로 변환할 수 없다고 함.
+    profile_character_id = serializers.PrimaryKeyRelatedField(
+        queryset=Character.objects.all(),
         write_only=True,
-        source='profile_character' # internal name 매핑 
-    )
+        source='profile_character'
+    ) 
+
     
     class Meta:
         model = User
