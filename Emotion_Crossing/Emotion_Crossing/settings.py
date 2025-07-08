02f4e116c6471c120890b6e454ee88088ebc13b3
dohyeong <daniel010309@gmail.com>
2025-07-08 16:36:57 +0900

    FIX:change static directory

--- Emotion_Crossing/Emotion_Crossing/settings.py
+++ Emotion_Crossing/Emotion_Crossing/settings.py
@@ -15,7 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', # .../Emotion_Crossing_BE/static
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
