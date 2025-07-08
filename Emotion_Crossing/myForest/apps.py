f2de6653e5b6573e7f84a2a92e5f31dd5b49197f
Ubuntu <ubuntu@ip-172-31-47-192.ap-northeast-2.compute.internal>
2025-07-08 10:08:10 +0000

    chore:remove tracked Python bytecode files

--- Emotion_Crossing/myForest/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyforestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myForest'
--- Emotion_Crossing/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import SignUpApiView, CurrentUserApiView,SignUpCharacterApiView
-
-urlpatterns = [
-    path('signup', SignUpApiView.as_view(), name='signup'),
-    path('mypage', CurrentUserApiView.as_view(), name='mypage'),
-    path('signup/characters', SignUpCharacterApiView.as_view(), name='signup-characters'),
-
-]
