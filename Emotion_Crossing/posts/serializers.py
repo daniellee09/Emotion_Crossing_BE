826cff47afc72e1ae24d20e66488862a055d8dd4
dohyeong <daniel010309@gmail.com>
2025-07-09 15:14:20 +0900

    FIX:posts get function change

--- Emotion_Crossing/posts/serializers.py
+++ Emotion_Crossing/posts/serializers.py
@@ -13,12 +13,8 @@ class TreeSerializer(serializers.ModelSerializer):
 ## 클라이언트가 넘긴 PK(UUID)를 받아서 그에 대응하는 Tree 인스턴스를 찾아 모델의 FK 필드에 넣어 주는 PrimaryKeyRelatedField를 사용해야 오류가 안 남
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True) ## source='user_id' 지정 시 불필요한 중복 지정 오류로 에러 발생해서 제거함 
-    # 수정사항 
-    # user_id = serializers.PrimaryKeyRelatedField( #사용자의 UUID를 받는 필드
-    #     queryset = User.objects.all(),
-    #     write_only=True,
-    #     help_text="작성자 UUID"
-    # )
+    user_name = serializers.CharField(source='user_id.name', read_only=True) # 작성자 이름만 따로 표시 
+    
     tree = TreeSerializer(read_only=True) ## 여기도 source='tree_id' 제거함 
     tree_id = serializers.PrimaryKeyRelatedField(
         queryset = Tree.objects.all(),
@@ -29,8 +25,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'post_id', 'user', 'tree', 'tree_id',
+            'post_id', 'user', 'user_name', 'tree', 'tree_id',
             'content', "is_private",
             'like_count', 'cheer_count', 'created_at'
         ]
-        read_only_fields = ['post_id', 'user', 'tree', 'like_count', 'cheer_count', 'created_at']
+        read_only_fields = ['post_id', 'user', 'user_name', 'tree', 'like_count', 'cheer_count', 'created_at']
