6cbbf6585ae5b8fc5068beceead7b76191fd4a67
bluelion10 <01cw@naver.com>
2025-07-09 16:00:08 +0900

    FEAT: 사용자의 작성글이 있는 나무의 UUID 조회기능 구현

--- Emotion_Crossing/myForest/views.py
+++ Emotion_Crossing/myForest/views.py
@@ -1,18 +1,22 @@
-from rest_framework import viewsets
-from .serializers import MyForestSerializer
-from .models import myForest
+# myforest/views.py
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from users.authentication import UserIDAuthentication
+from posts.models import Post
 
-# Create your views here.
-# GET 기록을 남긴 모든 나무 조회 
-class MyForestViewSet(viewsets.ReadOnlyModelViewSet):
-    # 내 숲 정보를 조회하는 ViewSet
-    # 클라이언트가 요청 헤더에 'X-USER-ID'를 포함해서 보내야 한다.
+class MyForestViewSet(ViewSet):
     authentication_classes = [UserIDAuthentication]
-    serializer_class = MyForestSerializer
-    
-    def get_queryset(self):
-        # 요청 헤더의 user_id를 기반으로 해당 사용자의 숲을 반환
-        return myForest.objects.filter(user_id=self.request.user)
-        
-        
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        user = request.user
+        tree_ids = (
+            Post.objects.filter(user_id=user)
+            .values_list('tree_id', flat=True)
+            .distinct()
+        )
+        if not tree_ids:
+            return Response({"message": "글을 작성한 나무가 없습니다", "tree_id": []})
+
+        return Response({"tree_id": list(tree_ids)})
