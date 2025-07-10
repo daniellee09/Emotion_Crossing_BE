b26518897bbf6e55be1ba3950e55646c3606c523
dohyeong <daniel010309@gmail.com>
2025-07-11 01:13:40 +0900

    All True

--- Emotion_Crossing/Emotion_Crossing/settings.py
+++ Emotion_Crossing/Emotion_Crossing/settings.py
@@ -67,15 +67,16 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://emotioncrossing.p-e.kr",
-    "http://emotioncrossing.p-e.kr",
-    "http://localhost:5173",
-    "https://localhost:5173",
-    "http://localhost:8000",
-    "https://localhost:8000",
-    "https://incandescent-kelpie-d2a6ad.netlify.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://emotioncrossing.p-e.kr",
+#     "http://emotioncrossing.p-e.kr",
+#     "http://localhost:5173",
+#     "https://localhost:5173",
+#     "http://localhost:8000",
+#     "https://localhost:8000",
+#     "https://incandescent-kelpie-d2a6ad.netlify.app",
+# ]
 
 CORS_ALLOWED_METHODS = list(default_methods)
 
